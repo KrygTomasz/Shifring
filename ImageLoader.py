b4046de50f3672b229d274c49966087aefec611c
szymongor <gornioczek.szymon@gmail.com>
2016-12-02 20:48:34 +0100

    Image loader and WaveletTransfrom
    Refactor main and new class ImageLoader (ImageProcessor?)
    Imgloader extract red green and blue channels to arrays.
    
    Problem with random color dots.

---
+++ ImageLoader.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+from PIL import Image
+
+class ImageLoader:
+
+    def __init__(self, imagePath):
+        self.image = Image.open(imagePath)
+
+    def getImage(self):
+        return self.image
+
+    def getRedChannel(self):
+        redChannel = self.image.split()[0].convert("L")
+        return redChannel
+
+    def getGreenChannel(self):
+        greenChannel = self.image.split()[1].convert("L")
+        return greenChannel
+
+    def getBlueChannel(self):
+        blueChannel = self.image.split()[2].convert("L")
+        return blueChannel
+
+    def getGrayLevel(self):
+        redChannel = self.image.convert("L")
+        return redChannel
+
+    def mergeChannelsToImg(self,redChannel,greenChannel,blueChannel):
+        width = redChannel[0].size
+        height = redChannel.size/width
+        channels = 3
+        img = np.zeros((height, width, channels), dtype=np.uint8)
+        for y in range(img.shape[0]):
+            for x in range(img.shape[1]):
+                r, g, b = (redChannel[y][x],greenChannel[y][x],blueChannel[y][x])
+                img[y][x][0] = r #self.boundValue(r)
+                img[y][x][1] = g #self.boundValue(g)
+                img[y][x][2] = b #self.boundValue(b)
+
+        return img
+
+#    def boundValue(self,value):
+#        if(value>254):
+#            return 254
+#        else:
+#            return value
--- WaveletTransform.py
+++ WaveletTransform.py
@@ -9,8 +9,21 @@ class WaveletTransform:
     def tranrform(self, image):
         coeffs2 = pywt.dwt2(image, 'haar')
         LL, (LH, HL, HH) = coeffs2
+        threshold = 3
+        self.lossyCompress(LH, threshold)
+        self.lossyCompress(HL, threshold)
+        self.lossyCompress(HH, threshold)
         return [LL, LH, HL, HH]
 
     def reconstruct(self, coeffs):
         coeffs2 = coeffs[0], (coeffs[1], coeffs[2], coeffs[3])
         return pywt.idwt2(coeffs2, 'haar')
+
+    def lossyCompress(self,dataArray,threshold):
+        width = dataArray[0].size
+        height = dataArray.size / width
+        for y in range(width):
+            for x in range(height):
+                if(dataArray[y][x]<threshold and dataArray[y][x]>-threshold):
+                    #print dataArray[y][x]
+                    dataArray[y][x] = 0
